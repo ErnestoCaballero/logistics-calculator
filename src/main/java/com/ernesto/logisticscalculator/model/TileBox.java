e7080a2a385e5273240e20aa95aef3da8755a825
Ernesto Caballero Delgado <ernesto.cab.del@gmail.com>
2022-12-20 00:05:22 -0600

    add table's and fields names using jakarta annotations

--- src/main/java/com/ernesto/logisticscalculator/model/TileBox.java
+++ src/main/java/com/ernesto/logisticscalculator/model/TileBox.java
@@ -1,28 +1,42 @@
 package com.ernesto.logisticscalculator.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@NoArgsConstructor
 @Entity
+@Table(name = "tile_boxes")
 public class TileBox {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(name = "code")
     private String code;
+
+    @Column(name = "description")
     private String description;
+
+    @Column(name = "m2_pero_box")
     private Double m2PerBox;
+
+    @Column(name = "boxes_per_pallet")
     private Integer boxesPerPallet;
+
+    @Column(name = "weight_per_box")
     private Double weightPerBox;
 
-
-
-
+    public TileBox(Long id, String code, String description, Double m2PerBox, Integer boxesPerPallet, Double weightPerBox) {
+        this.id = id;
+        this.code = code;
+        this.description = description;
+        this.m2PerBox = m2PerBox;
+        this.boxesPerPallet = boxesPerPallet;
+        this.weightPerBox = weightPerBox;
+    }
 }
