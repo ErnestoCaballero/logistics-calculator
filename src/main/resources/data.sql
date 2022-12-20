a450069b781198dcee9b17ef4bc14626dbec51f4
Ernesto Caballero Delgado <ernesto.cab.del@gmail.com>
2022-12-20 00:21:40 -0600

    add initial data to tile_boxes table

---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO tile_boxes (code, description, m2_per_box, boxes_per_pallet, weight_per_box)
+VALUES ('PP022', 'Trentto', 1.44, 30, 22.34);
+INSERT INTO tile_boxes (code, description, m2_per_box, boxes_per_pallet, weight_per_box)
+VALUES ('PP025', 'Doratta', 1.44, 32, 21.8);
+INSERT INTO tile_boxes (code, description, m2_per_box, boxes_per_pallet, weight_per_box)
+VALUES ('PP027', 'Scherzo', 1.44, 28, 25.21);
