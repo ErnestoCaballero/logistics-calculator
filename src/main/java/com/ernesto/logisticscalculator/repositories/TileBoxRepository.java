6741f867629f2ab5f63c17c6d1bdd7341666ff18
Ernesto Caballero Delgado <ernesto.cab.del@gmail.com>
2022-12-27 22:07:08 -0600

    add TileBoxRepository to be able to use Spring Data JPA. Closes #2

---
+++ src/main/java/com/ernesto/logisticscalculator/repositories/TileBoxRepository.java
@@ -0,0 +1,8 @@
+package com.ernesto.logisticscalculator.repositories;
+
+import com.ernesto.logisticscalculator.model.TileBox;
+import org.springframework.data.repository.CrudRepository;
+
+public interface TileBoxRepository extends CrudRepository<TileBox, Long> {
+
+}
