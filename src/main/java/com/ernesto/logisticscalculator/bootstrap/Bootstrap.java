c2ae60c73371cea1da9774f88c3f32668fa6d33c
Ernesto Caballero Delgado <ernesto.cab.del@gmail.com>
2023-01-05 01:18:38 -0600

    create bootstrap class to initialize some data on DB. Closes #6

---
+++ src/main/java/com/ernesto/logisticscalculator/bootstrap/Bootstrap.java
@@ -0,0 +1,34 @@
+package com.ernesto.logisticscalculator.bootstrap;
+
+import com.ernesto.logisticscalculator.model.TileBox;
+import com.ernesto.logisticscalculator.repositories.TileBoxRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+@Slf4j
+@Component
+public class Bootstrap implements ApplicationListener<ContextRefreshedEvent> {
+
+    public TileBoxRepository tileBoxRepository;
+
+    public Bootstrap(TileBoxRepository tileBoxRepository) {
+        this.tileBoxRepository = tileBoxRepository;
+    }
+
+    @Override
+    @Transactional
+    public void onApplicationEvent(ContextRefreshedEvent event) {
+        TileBox tileBox1 = new TileBox();
+
+        tileBox1.setCode("PP059");
+        tileBox1.setDescription("Colossale");
+        tileBox1.setM2PerBox(2.88);
+        tileBox1.setBoxesPerPallet(20);
+        tileBox1.setWeightPerBox(23.9);
+
+        tileBoxRepository.save(tileBox1);
+    }
+}
