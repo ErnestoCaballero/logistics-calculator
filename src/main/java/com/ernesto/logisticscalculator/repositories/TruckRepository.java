e37ce98121f74e59d4004e309fba45eedae95f58
Ernesto Caballero Delgado <ernesto.cab.del@gmail.com>
2023-01-06 00:54:13 -0600

    create TruckRepository. Closes #12

---
+++ src/main/java/com/ernesto/logisticscalculator/repositories/TruckRepository.java
@@ -0,0 +1,8 @@
+package com.ernesto.logisticscalculator.repositories;
+
+import com.ernesto.logisticscalculator.model.Truck;
+import org.springframework.data.repository.CrudRepository;
+
+public interface TruckRepository extends CrudRepository<Truck, Long> {
+
+}
