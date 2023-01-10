e4d37816742c6394b6135db37ee56d521e6a8fe4
Ernesto Caballero Delgado <ernesto.cab.del@gmail.com>
2023-01-10 01:22:35 -0600

    add initial data for truck types

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -4,3 +4,9 @@ INSERT INTO tile_boxes (code, description, m2_per_box, boxes_per_pallet, weight_
 VALUES ('PP025', 'Doratta', 1.44, 32, 21.8);
 INSERT INTO tile_boxes (code, description, m2_per_box, boxes_per_pallet, weight_per_box)
 VALUES ('PP027', 'Scherzo', 1.44, 28, 25.21);
+INSERT INTO trucks (capacity_in_tons,cost_per_trip,type)
+VALUES (30.0, 25000.0,'Light');
+INSERT INTO trucks (capacity_in_tons,cost_per_trip,type)
+VALUES (42.0, 34500.0,'Mid');
+INSERT INTO trucks (capacity_in_tons,cost_per_trip,type)
+VALUES (55.0, 42000.0,'Heavy');
