6fda0608e3a001dbe43128d85e276675d28ae1df
Ernesto Caballero Delgado <ernesto.cab.del@gmail.com>
2023-01-10 01:27:40 -0600

    Update index Controller by adding attributes to the model. Closes #9

--- src/main/java/com/ernesto/logisticscalculator/controllers/IndexController.java
+++ src/main/java/com/ernesto/logisticscalculator/controllers/IndexController.java
@@ -1,16 +1,26 @@
 package com.ernesto.logisticscalculator.controllers;
 
+import com.ernesto.logisticscalculator.services.TruckService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Slf4j
 @Controller
 public class IndexController {
 
+    private final TruckService truckService;
+
+    public IndexController(TruckService truckService) {
+        this.truckService = truckService;
+    }
+
     @RequestMapping({"", "/", "/index"})
-    public String getIndexPage() {
+    public String getIndexPage(Model model) {
         log.debug("Getting index page...");
+
+        model.addAttribute("trucks", truckService.findAll());
 
         return "index";
     }
