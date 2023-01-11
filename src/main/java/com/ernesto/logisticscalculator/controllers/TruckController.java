fb8569ac6edf8a30377ffca215b70ec7ee435432
Ernesto Caballero Delgado <ernesto.cab.del@gmail.com>
2023-01-11 00:54:02 -0600

    create TruckController. Closes #14

---
+++ src/main/java/com/ernesto/logisticscalculator/controllers/TruckController.java
@@ -0,0 +1,19 @@
+package com.ernesto.logisticscalculator.controllers;
+
+import com.ernesto.logisticscalculator.services.TruckService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Slf4j
+@Controller
+public class TruckController {
+
+    private final TruckService truckService;
+
+    public TruckController(TruckService truckService) {
+        this.truckService = truckService;
+    }
+}
