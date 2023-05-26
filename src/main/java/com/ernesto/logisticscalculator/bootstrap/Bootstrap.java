b4c01283393abfbf03d3002225ad84325f1338c1
Ernesto Caballero Delgado <ernesto.cab.del@gmail.com>
2023-05-26 15:10:14 -0600

    add first trip enty to db

--- src/main/java/com/ernesto/logisticscalculator/bootstrap/Bootstrap.java
+++ src/main/java/com/ernesto/logisticscalculator/bootstrap/Bootstrap.java
@@ -1,13 +1,24 @@
 package com.ernesto.logisticscalculator.bootstrap;
 
 import com.ernesto.logisticscalculator.model.TileBox;
+import com.ernesto.logisticscalculator.model.Trip;
+import com.ernesto.logisticscalculator.model.TripDetail;
+import com.ernesto.logisticscalculator.model.Truck;
 import com.ernesto.logisticscalculator.repositories.TileBoxRepository;
+import com.ernesto.logisticscalculator.repositories.TripDetailRepository;
+import com.ernesto.logisticscalculator.repositories.TripRepository;
+import com.ernesto.logisticscalculator.services.TileBoxService;
+import com.ernesto.logisticscalculator.services.TileBoxServiceImpl;
+import com.ernesto.logisticscalculator.services.TruckService;
+import jdk.swing.interop.SwingInterOpUtils;
 import lombok.extern.slf4j.Slf4j;
+import org.aspectj.apache.bcel.generic.LineNumberGen;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Date;
 import java.util.Optional;
 import java.util.Scanner;
 
@@ -16,9 +27,17 @@ import java.util.Scanner;
 public class Bootstrap implements ApplicationListener<ContextRefreshedEvent> {
 
     public TileBoxRepository tileBoxRepository;
+    public TileBoxService tileBoxService;
+    private final TripRepository tripRepository;
+    private final TripDetailRepository tripDetailRepository;
+    private final TruckService truckService;
 
-    public Bootstrap(TileBoxRepository tileBoxRepository) {
+    public Bootstrap(TileBoxRepository tileBoxRepository, TileBoxService tileBoxService, TripRepository tripRepository, TripDetailRepository tripDetailRepository, TruckService truckService) {
         this.tileBoxRepository = tileBoxRepository;
+        this.tileBoxService = tileBoxService;
+        this.tripRepository = tripRepository;
+        this.tripDetailRepository = tripDetailRepository;
+        this.truckService = truckService;
     }
 
     @Override
@@ -34,18 +53,39 @@ public class Bootstrap implements ApplicationListener<ContextRefreshedEvent> {
 
         tileBoxRepository.save(tileBox1);
 
-//        Scanner scanner = new Scanner(System.in);
-//
-//        System.out.println("Please insert the id of the item you want to find");
-//        Long id = scanner.nextLong();
-//
-//        Optional<TileBox> optionalTileBox = tileBoxRepository.findById(id);
-//
-//        System.out.println("The description of the item is: " + optionalTileBox.get().getDescription());
-//
-//        System.out.println("Select the id of the item to be deleted: ");
-//        Long idToDelete = scanner.nextLong();
-//        tileBoxRepository.deleteById(idToDelete);
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.println("Let's crate now a new TileBox using the TileBoxService...");
+        TileBox tileBox2 = new TileBox();
+
+        System.out.println("Instantiation finish. Adding attributes...");
+        tileBox2.setCode("CM02");
+        tileBox2.setDescription("Cementi");
+        tileBox2.setM2PerBox(1.44);
+        tileBox2.setBoxesPerPallet(22);
+        tileBox2.setWeightPerBox(25.8);
+
+        System.out.println("Saving the new product using TileBoxService...");
+        tileBoxService.save(tileBox2);
+
+        // Create a Trip
+        System.out.println("Let's now create a Trip");
+        System.out.println("Indicate the truck id you're sending: ");
+        Long truck_id = scanner.nextLong();
+        Truck truck = truckService.findById(truck_id);
+
+        Trip trip = new Trip();
+        trip.setDate(new Date());
+        trip.setTotalCargoWeight(0.0);
+        trip.setTotalSquareMeters(0.0);
+        trip.setTripCost(truck.getCost());
+        trip.setTruckCapacity(truck.getCapacityInTons());
+        trip.setTruck(truck);
+
+        tripRepository.save(trip);
+
+        System.out.println("checking the newly generated trip id...");
+        Long newTripId = trip.getId();
+        System.out.println("The id for the new trip generated is " + trip.getId());
     }
 }
