1b4b376be904e086ef1a8bb1874469363e0fb178
Ernesto Caballero Delgado <ernesto.cab.del@gmail.com>
2023-06-01 13:17:50 -0600

    add Model to startTrip()

--- src/main/java/com/ernesto/logisticscalculator/controllers/TripController.java
+++ src/main/java/com/ernesto/logisticscalculator/controllers/TripController.java
@@ -4,8 +4,11 @@ import com.ernesto.logisticscalculator.repositories.TripRepository;
 import com.ernesto.logisticscalculator.services.TruckService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Slf4j
 @Controller
@@ -19,10 +22,22 @@ public class TripController {
         this.tripRepository = tripRepository;
     }
 
-    @GetMapping("/trip/{truckId}/start")
-    public String startTrip(@PathVariable String truckId) {
+    @GetMapping("/trip/{truckId}/start/{itemsNumber}")
+    public String startTrip(@PathVariable String truckId, @PathVariable int itemsNumber, Model model) {
         log.debug("Enter the startTrip() method");
+        log.debug("The truckId is: " + truckId + " and the itemsNumber is: " + itemsNumber);
+        
+        model.addAttribute("itemsNumber", itemsNumber);
 
         return "entities/trip/tripform";
+    }
+
+    @PostMapping("trip/{truckId}/submit")
+    public String submitItems(@PathVariable String truckId, @RequestParam("itemsNumber") int itemsNumber) {
+        log.debug("Enter the submitItems() method");
+        log.debug("The inputted items to send are: " + itemsNumber);
+        log.debug("The truck id is: " + truckId);
+
+        return "redirect:/trip/" + truckId + "/start/" + itemsNumber;
     }
 }
