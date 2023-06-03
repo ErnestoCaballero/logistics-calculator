262d23513a81211a62212027158d8ac685c0040d
Ernesto Caballero Delgado <ernesto.cab.del@gmail.com>
2023-06-02 19:12:14 -0600

    persisting to db the trip and null values in TripDetails

--- src/main/java/com/ernesto/logisticscalculator/controllers/TripController.java
+++ src/main/java/com/ernesto/logisticscalculator/controllers/TripController.java
@@ -1,5 +1,8 @@
 package com.ernesto.logisticscalculator.controllers;
 
+import com.ernesto.logisticscalculator.model.Trip;
+import com.ernesto.logisticscalculator.model.TripDetail;
+import com.ernesto.logisticscalculator.model.Truck;
 import com.ernesto.logisticscalculator.repositories.TripRepository;
 import com.ernesto.logisticscalculator.services.TruckService;
 import lombok.extern.slf4j.Slf4j;
@@ -9,6 +12,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 @Slf4j
 @Controller
@@ -26,8 +33,40 @@ public class TripController {
     public String startTrip(@PathVariable String truckId, @PathVariable int itemsNumber, Model model) {
         log.debug("Enter the startTrip() method");
         log.debug("The truckId is: " + truckId + " and the itemsNumber is: " + itemsNumber);
-        
+
+        // Add itemsNumber to Model to create as many entries in tripform.html as required
         model.addAttribute("itemsNumber", itemsNumber);
+
+        // Create new Trip
+        log.debug("Creating new Trip");
+        Truck truck = truckService.findById(Long.valueOf(truckId));
+
+        Trip trip = new Trip();
+        trip.setDate(new Date());
+        trip.setTotalCargoWeight(0.0);
+        trip.setTotalSquareMeters(0.0);
+        trip.setTripCost(truck.getCost());
+        trip.setTruckCapacity(truck.getCapacityInTons());
+        trip.setTruck(truck);
+
+        // Setting the trip details List to be populated after the submission of the tripform.html
+        List<TripDetail> tripDetails = new ArrayList<>();
+        for (int i = 0; i < itemsNumber; i++) {
+            TripDetail tripDetail = new TripDetail();
+            tripDetail.setTrip(trip);
+
+            tripDetails.add(tripDetail);
+        }
+
+        trip.setTripDetails(tripDetails);
+
+        model.addAttribute("tripDetails", tripDetails);
+
+        model.addAttribute("tripId", trip.getId());
+
+        tripRepository.save(trip);
+
+        log.debug("Trip created with id: " + trip.getId());
 
         return "entities/trip/tripform";
     }
@@ -39,5 +78,11 @@ public class TripController {
         log.debug("The truck id is: " + truckId);
 
         return "redirect:/trip/" + truckId + "/start/" + itemsNumber;
+    }
+
+    @PostMapping("trip/save")
+    public String saveTripDetails() {
+
+        return "redirect:/";
     }
 }
