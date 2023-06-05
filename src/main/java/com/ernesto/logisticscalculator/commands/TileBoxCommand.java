b1ca5b292ba740b6ea78ca054a6ed20eb5c24c65
Ernesto Caballero Delgado <ernesto.cab.del@gmail.com>
2023-06-05 14:47:32 -0600

    create TileBoxCommand to perform CRUD

--- src/main/java/com/ernesto/logisticscalculator/commands/TileBoxCommand.java
+++ src/main/java/com/ernesto/logisticscalculator/commands/TileBoxCommand.java
@@ -1,2 +1,17 @@
-package com.ernesto.logisticscalculator.commands;public class TileBoxCommand {
+package com.ernesto.logisticscalculator.commands;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@NoArgsConstructor
+public class TileBoxCommand {
+    private Long id;
+    private String code;
+    private String description;
+    private Double m2PerBox;
+    private Integer boxPerPallet;
+    private Double weightPerBox;
 }
