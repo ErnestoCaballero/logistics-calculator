ba917a4121d784b5e56b1873eef7680d46b44df0
Ernesto Caballero Delgado <ernesto.cab.del@gmail.com>
2023-06-16 12:46:01 -0600

    add delete_ind field to initial data

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,9 +1,9 @@
-INSERT INTO tile_boxes (code, description, m2_per_box, boxes_per_pallet, weight_per_box)
-VALUES ('PP022', 'Trentto', 1.44, 30, 22.34);
-INSERT INTO tile_boxes (code, description, m2_per_box, boxes_per_pallet, weight_per_box)
-VALUES ('PP025', 'Doratta', 1.44, 32, 21.8);
-INSERT INTO tile_boxes (code, description, m2_per_box, boxes_per_pallet, weight_per_box)
-VALUES ('PP027', 'Scherzo', 1.44, 28, 25.21);
+INSERT INTO tile_boxes (code, description, m2_per_box, boxes_per_pallet, weight_per_box, deleted_ind)
+VALUES ('PP022', 'Trentto', 1.44, 30, 22.34, FALSE);
+INSERT INTO tile_boxes (code, description, m2_per_box, boxes_per_pallet, weight_per_box, deleted_ind)
+VALUES ('PP025', 'Doratta', 1.44, 32, 21.8, FALSE);
+INSERT INTO tile_boxes (code, description, m2_per_box, boxes_per_pallet, weight_per_box, deleted_ind)
+VALUES ('PP027', 'Scherzo', 1.44, 28, 25.21, FALSE);
 INSERT INTO trucks (capacity_in_tons,cost_per_trip,type)
 VALUES (30.0, 25000.0,'Light');
 INSERT INTO trucks (capacity_in_tons,cost_per_trip,type)
