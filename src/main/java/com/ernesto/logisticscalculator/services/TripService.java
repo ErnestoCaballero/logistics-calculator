583297eef4468d53e04d44549413479c2a51c847
Ernesto Caballero Delgado <ernesto.cab.del@gmail.com>
2023-06-26 18:34:38 -0600

    create TripService interface

---
+++ src/main/java/com/ernesto/logisticscalculator/services/TripService.java
@@ -0,0 +1,19 @@
+package com.ernesto.logisticscalculator.services;
+
+import com.ernesto.logisticscalculator.model.Trip;
+import com.ernesto.logisticscalculator.repositories.TripRepository;
+
+import java.util.ArrayList;
+
+public interface TripService {
+
+    ArrayList<Trip> findAll();
+
+    Trip save(Trip trip);
+
+    Trip findById(Long id);
+
+    void deleteById(Long id);
+
+    void delete(Trip trip);
+}
