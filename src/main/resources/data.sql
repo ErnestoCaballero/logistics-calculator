6249272b35d2aa2b512a263fc5a23b3234d5dbaa
Ernesto Caballero Delgado <ernesto.cab.del@gmail.com>
2023-07-04 10:24:49 -0600

    add isDeleted to data initialization

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -4,9 +4,9 @@ INSERT INTO tile_boxes (code, description, m2_per_box, boxes_per_pallet, weight_
 VALUES ('PP025', 'Doratta', 1.44, 32, 21.8, FALSE);
 INSERT INTO tile_boxes (code, description, m2_per_box, boxes_per_pallet, weight_per_box, deleted_ind)
 VALUES ('PP027', 'Scherzo', 1.44, 28, 25.21, FALSE);
-INSERT INTO trucks (capacity_in_tons,cost_per_trip,type)
-VALUES (30.0, 25000.0,'Light');
-INSERT INTO trucks (capacity_in_tons,cost_per_trip,type)
-VALUES (42.0, 34500.0,'Mid');
-INSERT INTO trucks (capacity_in_tons,cost_per_trip,type)
-VALUES (55.0, 42000.0,'Heavy');
+INSERT INTO trucks (capacity_in_tons,cost_per_trip,type,deleted_ind)
+VALUES (30.0, 25000.0,'Light',FALSE);
+INSERT INTO trucks (capacity_in_tons,cost_per_trip,type,deleted_ind)
+VALUES (42.0, 34500.0,'Mid',FALSE);
+INSERT INTO trucks (capacity_in_tons,cost_per_trip,type,deleted_ind)
+VALUES (55.0, 42000.0,'Heavy',FALSE);
